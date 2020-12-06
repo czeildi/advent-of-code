19ad6f5207a567300bde40e399f0a02f3b69175a
Ildikó Czeller <czeildi@gmail.com>
2020-12-06 10:02:54 +0000

    add day02 solution

---
+++ solutions/day02.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+input <- readr::read_lines("solutions/day02_input.txt")
+passwords <- tibble(input = input) %>% 
+  separate(input, c("min", "max", "letter", "password"), convert = TRUE) 
+
+
+# part 1 ------------------------------------------------------------------
+
+passwords %>% 
+  mutate(is_correct = pmap_lgl(., function(min, max, letter, password) {
+    min <= str_count(password, letter) && max >= str_count(password, letter)
+  })) %>% 
+  count(is_correct)
+
+
+# part 2 ------------------------------------------------------------------
+
+passwords %>% 
+  mutate(is_correct = pmap_lgl(., function(min, max, letter, password) {
+    password_chars <- str_split(password, '')[[1]]
+    ((password_chars[min] == letter) + (password_chars[max] == letter)) == 1
+  })) %>% 
+  count(is_correct)
