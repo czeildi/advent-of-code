e2c635533bab3cde0a58dab729bc471a717117e1
Ildikó Czeller <czeildi@gmail.com>
2020-12-06 09:41:06 +0000

    add day01 solution

--- solutions/day01.R
+++ solutions/day01.R
@@ -1,1 +1,13 @@
 library(tidyverse)
+
+numbers <- as.numeric(readr::read_lines("solutions/day01_input.txt"))
+
+for (x in numbers) {
+  for (y in numbers) {
+    for(z in numbers) {
+      if ((x + y + z) == 2020) {
+        print(x*y*z) 
+      }
+    }
+  }
+}
