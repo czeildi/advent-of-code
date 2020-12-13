77afecedde51aff55f14aa0dc8e651d3ec8280ff
Ildikó Czeller <czeildi@gmail.com>
2020-12-13 08:51:27 +0000

    add day13 solution part 2

--- solutions/day13.R
+++ solutions/day13.R
@@ -24,3 +24,36 @@ while(length(catched_buses(departure_time)) == 0) {
 }
 
 catched_buses(departure_time) * (departure_time - arrival_time)
+
+
+# part 2 ------------------------------------------------------------------
+
+# sample solution: 1068781 
+
+schedules <- tibble(freq = input[2]) %>%
+  separate_rows(freq) %>%
+  mutate(target_diff = 1:n() - 1) %>%
+  filter(freq != 'x') %>%
+  mutate(freq = as.numeric(freq)) %>%
+  mutate(remainder_at_departure = (freq - target_diff) %% freq) %>%
+  arrange(freq)
+
+remainder_of_product <- function(base1, base2, r1, r2) {
+  candidate <- r1
+  while(candidate %% base2 != r2) {
+    candidate <- candidate + base1
+  }
+  return(candidate)
+}
+
+base1 <- schedules$freq[1]
+r1 <- schedules$remainder_at_departure[1]
+
+for (row_index in 2:nrow(schedules)) {
+  base2 <- schedules$freq[row_index]
+  r2 <- schedules$remainder_at_departure[row_index]
+  r1 <<- remainder_of_product(base1, base2, r1, r2)
+  base1 <<- base1 * base2
+}
+
+sprintf("%.100f", r1)
