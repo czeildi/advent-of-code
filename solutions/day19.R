798f9d357f9e50f86a3534959142350095cc0eed
Ildikó Czeller <czeildi@gmail.com>
2020-12-19 07:38:19 +0000

    add day19 part 2 solution

--- solutions/day19.R
+++ solutions/day19.R
@@ -31,9 +31,29 @@ rule_regex <- function(rule_idx) {
   }
   if (rule_idx == 0) {
     return(str_c("^", new_rule, "$"))
+  } else if (rule_idx == 8) {
+    return(str_c("(", new_rule, ")+"))
+  } else if (rule_idx == 11) {
+    return(rule_for_11())
   } else {
     return(new_rule)
   }
+}
+
+n_times_regex <- function(n) {
+  str_c("{", n, ",", n, "}")
+}
+
+rule_for_11 <- function() {
+  rule <- rules[12]
+  parts <- str_split(rule, " ") %>% .[[1]] %>% as.integer()
+  regex_subparts <- parts %>% 
+    map_chr(m_rule_regex) %>% 
+    map(~str_c("(", .x, ")"))
+  # 8 is by trial and error
+  map_chr(1:8, ~str_c(regex_subparts[1], n_times_regex(.x), regex_subparts[2], n_times_regex(.x))) %>%
+    map(~str_c("(", .x, ")")) %>%
+    str_c(collapse = "|") 
 }
 
 m_rule_regex <- memoise::memoise(rule_regex)
