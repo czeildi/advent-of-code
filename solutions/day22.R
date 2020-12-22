8fa99fd69243fec7435b1b6a8f15f6de9899908a
Ildikó Czeller <czeildi@gmail.com>
2020-12-22 20:15:10 +0000

    refactor day22 part2

--- solutions/day22.R
+++ solutions/day22.R
@@ -1,26 +1,46 @@
 library(profvis)
 library(tidyverse)
 
-input <- tibble(x = read_file("solutions/day22_input.txt")) %>%
-  separate_rows(x, sep = "\n\n") %>%
+input <- tibble(cards = read_file("solutions/day22_input.txt")) %>%
+  separate_rows(cards, sep = "\n\n") %>%
   mutate(player_idx = 1:n()) %>%
-  mutate(x = str_replace_all(x, "Player \\d:", "")) %>%
-  separate_rows(x, sep = "\n", convert = TRUE) %>%
-  filter(x != "")
+  mutate(cards = str_replace_all(cards, "Player \\d:", "")) %>%
+  separate_rows(cards, sep = "\n", convert = TRUE) %>%
+  filter(cards != "")
 
-cards <- group_by(input, player_idx) %>% summarize(card_values = list(x)) %>% deframe()
-
+cards <- list(
+  '1' = filter(input, player_idx == 1)$cards,
+  '2' = filter(input, player_idx == 2)$cards
+)
 # global variable to keep track of repeated dealings
-# 20 is a guess at max game depth
-seen_card_dealings <- map(1:20, ~character(0))
+seen_card_dealings <- list()
 
-should_game_proceed <- function(cards, game_id, step_id_in_game) {
+
+play_game <- function(cards, game_depth) {
+  print(glue::glue("Game {game_depth} level deep"))
+  seen_card_dealings[[as.character(game_depth)]] <- character(0)
+  round_id <- 1
+  
+  should_proceed <- should_game_proceed(cards, game_depth, round_id)
+  while (should_proceed$should_proceed) {
+    winner_id <- round_winner(cards, game_depth)
+    cards <- move_drawn_cards_to_winner_deck(cards, winner_id)
+    
+    round_id <- round_id + 1
+    should_proceed <- should_game_proceed(cards, game_depth, round_id)
+  }
+  
+  if (should_proceed$end_game_type == 'instant_end') winner_id <- 1
+  
+  return(list(winner_id = winner_id, cards = cards))
+}
+
+should_game_proceed <- function(cards, game_depth, round_id) {
   card_dealing_id <- digest::digest(cards)
-  if (card_dealing_id %in% seen_card_dealings[[game_id]]) {
-    # print("ending due to repeated dealing")
+  if (card_dealing_id %in% seen_card_dealings[[as.character(game_depth)]]) {
     return(list(should_proceed = FALSE, end_game_type = "instant_end"))
   }
-  seen_card_dealings[[game_id]][step_id_in_game] <<- card_dealing_id
+  seen_card_dealings[[as.character(game_depth)]][round_id] <<- card_dealing_id
   
   list(
     should_proceed = length(cards[[1]]) > 0 && length(cards[[2]]),
@@ -28,19 +48,22 @@ should_game_proceed <- function(cards, game_id, step_id_in_game) {
   )
 }
 
-should_play_subgame <- function(cards) {
-  cards[[1]][1] <= length(cards[[1]]) - 1 && cards[[2]][1] <= length(cards[[2]]) - 1
+round_winner <- function(cards, game_depth) {
+  if (!should_play_subgame(cards)) return(player_id_with_top_card(cards))
+
+  cards_for_subgame <- map(cards, ~tail(., -1)[1:(.[1])])
+  subgame_result <- play_game(cards_for_subgame, game_depth + 1)
+  subgame_result$winner_id
 }
 
-cards_for_subgame <- function(cards) {
-  map(cards, ~tail(., -1)[1:(.[1])])
+should_play_subgame <- function(cards) {
+  cards[[1]][1] <= length(cards[[1]]) - 1 && cards[[2]][1] <= length(cards[[2]]) - 1
 }
 
 player_id_with_top_card <- function(cards) { 
   if (cards[[1]][1] > cards[[2]][1]) return(1)
   return(2)
 }
-  
 
 move_drawn_cards_to_winner_deck <- function(cards, winner_id) {
   if (winner_id == 1) {
@@ -54,32 +77,6 @@ move_drawn_cards_to_winner_deck <- function(cards, winner_id) {
       '2' = c(tail(cards[[2]], -1), cards[[2]][1], cards[[1]][1])
     ))
   }
-}
-
-play_game <- function(cards, game_id) {
-  print(glue::glue("Game {game_id} level deep"))
-  
-  step_id_in_game <- 1
-  seen_card_dealings[[game_id]] <- character(0)
-  
-  should_proceed <- should_game_proceed(cards, game_id, step_id_in_game)
-  while (should_proceed$should_proceed) {
-    # print(glue::glue("Game {game_id} level deep, round {step_id_in_game}"))
-    
-    if(should_play_subgame(cards)) {
-      winner_id <- play_game(cards_for_subgame(cards), game_id + 1)$winner_id
-    } else {
-      winner_id <- player_id_with_top_card(cards)
-    } 
-    cards <- move_drawn_cards_to_winner_deck(cards, winner_id)
-    
-    step_id_in_game <- step_id_in_game + 1
-    should_proceed <- should_game_proceed(cards, game_id, step_id_in_game)
-  }
-  
-  if (should_proceed$end_game_type == 'instant_end') winner_id <- 1
-  
-  return(list(winner_id = winner_id, cards = cards))
 }
 
 system.time({
