184ec31c688d5465883f48e6c6d510460e899ec8
Ildikó Czeller <czeildi@gmail.com>
2020-12-23 08:05:40 +0000

    simplify a bit

--- solutions/day23.R
+++ solutions/day23.R
@@ -7,15 +7,10 @@ original_nums <- as.integer(str_split(test_input, "")[[1]])
 max_cup_value <- 10000
 n_round <- 1000000
 
-cup_values <- c(original_nums, 10:max_cup_value)
-cups <- tibble(cup = cup_values) %>% 
-  mutate(
-    right_cup = lead(cup, default = cup_values[1])
-  ) %>%
-  pull(right_cup)
+cups <- c(original_nums[2:9], 10:max_cup_value, original_nums[1])
 
 # profvis({
-current_cup <- cup_values[1]
+current_cup <- original_nums[1]
 
 step_destination_cup <- function(cup_label, picked_cups) {
   dest <- cup_label - 1
@@ -50,5 +45,5 @@ system.time({
 
 cups[1] * cups[cups[1]]
 
-# 10.000 100.000 1.8sec
-# 10.000 1.000.000 17sec
+# 10.000 100.000 1.3sec
+# 10.000 1.000.000 13sec
