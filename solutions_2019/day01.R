73e8e8331891def083dd2b0d53f1b1eece46e82a
Ildikó Czeller <czeildi@gmail.com>
2020-12-26 17:20:26 +0000

    add 2019 day 01 part 02

--- solutions_2019/day01.R
+++ solutions_2019/day01.R
@@ -1,6 +1,30 @@
 library(tidyverse)
 
-tibble(mass = as.numeric(read_lines("solutions_2019/day01_input.txt"))) %>%
+module_masses <- tibble(mass = as.numeric(read_lines("solutions_2019/day01_input.txt")))
+
+# part 1 ------------------------------------------------------------------
+
+module_masses %>%
   mutate(fuel = floor(mass / 3) - 2) %>% 
   pull(fuel) %>% 
   sum()
+
+# part 2 ------------------------------------------------------------------
+
+fuel_for_mass <- function(mass) {
+  accumulate(c(mass, rep(-1, 100)), function(fuel_mass, dummy) {
+    needed_fuel <- floor(fuel_mass / 3) - 2
+    if (needed_fuel <= 0) return(done(0))
+    needed_fuel
+  }) %>% 
+    tail(-1) %>%
+    sum()
+  
+}
+
+module_masses %>%
+  rowwise() %>%
+  mutate(fuel = fuel_for_mass(mass)) %>% 
+  pull(fuel) %>% 
+  sum()
+
