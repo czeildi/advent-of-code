e4f32a34f8b2d336bbab24d3197a4beb59ee7224
Ildiko Czeller <czeildi@gmail.com>
2020-12-29 19:07:28 +0100

    add 2019 day06 part 02

--- solutions_2019/day06_universal_orbit_map.R
+++ solutions_2019/day06_universal_orbit_map.R
@@ -5,6 +5,8 @@ orbits <- tibble(edge = read_lines("solutions_2019/day06_input.txt")) %>%
 
 centers_of_object_orbits <- deframe(select(orbits, orbiter, center))
 
+# part 1 -----------------------------------------------------------------------
+
 n_orbit_to_object <- function(object) {
   if (object == "COM") return(0)
   memoised_n_orbit_to_object(centers_of_object_orbits[[object]]) + 1
@@ -17,3 +19,19 @@ orbits %>%
   mutate(n_orbit = memoised_n_orbit_to_object(orbiter)) %>%
   pull(n_orbit) %>%
   sum()
+
+# part 2 -----------------------------------------------------------------------
+
+route_to_com <- function(object) {
+  if (object == "COM") return(c("COM" = 0))
+  current <- 0
+  names(current) <- object
+  c(current, route_to_com(centers_of_object_orbits[[object]]) + 1)
+}
+
+san_route <- route_to_com("SAN")
+you_route <- route_to_com("YOU")
+
+first_common_center <- intersect(names(san_route), names(you_route))[1]
+
+san_route[first_common_center] - 1 + you_route[first_common_center] - 1
