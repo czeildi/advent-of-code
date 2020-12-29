66f6f456bc07046c1813e3cc2bcaef0ee1b98674
Ildiko Czeller <czeildi@gmail.com>
2020-12-29 17:31:29 +0100

    add 2019 day05 part 02

--- solutions_2019/day05_sunny_witha_chanceof_asteroids.R
+++ solutions_2019/day05_sunny_witha_chanceof_asteroids.R
@@ -5,6 +5,8 @@ program <- read_lines("solutions_2019/day05_input.txt") %>%
   .[[1]] %>%
   as.numeric()
 
+PROGRAM_INPUT <- 5
+
 commands <- list(
   "1" = list(
     "n_param" = 3,
@@ -21,16 +23,50 @@ commands <- list(
   "3" = list(
     "n_param" = 1,
     "op" = function(raw_params, values) {
-      program[raw_params[1] + 1] <<- 1
+      program[raw_params[1] + 1] <<- PROGRAM_INPUT
     }
   ),
- "4" = list(
+  "4" = list(
     "n_param" = 1,
     "op" = function(raw_params, values) {
       print(values[1])
     }
- )
+  ),
+  "5" = list(
+    "n_param" = 2,
+    "op" = function(raw_params, values) { },
+    "step_index" = function(raw_params, values) {
+      if (values[1] != 0) values[2] + 1
+      else NULL
+    }
+  ),
+  "6" = list(
+    "n_param" = 2,
+    "op" = function(raw_params, values) { },
+    "step_index" = function(raw_params, values) {
+      if (values[1] == 0) values[2] + 1
+      else NULL
+    }
+  ),
+  "7" = list(
+    "n_param" = 3,
+    "op" = function(raw_params, values) {
+      comparison_result <- if_else(values[1] < values[2], 1, 0)
+      program[raw_params[3] + 1] <<- comparison_result
+    }
+  ),
+  "8" = list(
+    "n_param" = 3,
+    "op" = function(raw_params, values) {
+      comparison_result <- if_else(values[1] == values[2], 1, 0)
+      program[raw_params[3] + 1] <<- comparison_result
+    }
+  )
 )
+
+default_step_index <- function(idx, n_param) {
+  idx + 1 + n_param
+}
 
 idx <- 1
 
@@ -46,5 +82,14 @@ while (program[idx] %% 100 != 99) {
     else .x
   })
   command$op(raw_params, param_values)
-  idx <- idx + 1 + command$n_param
+  if (!is.null(command$step_index)) {
+    potential_new_idx <- command$step_index(raw_params, param_values)
+    if (is.null(potential_new_idx)) {
+      idx <- default_step_index(idx, command$n_param)
+    } else {
+      idx <- potential_new_idx
+    }
+  } else {
+    idx <- default_step_index(idx, command$n_param)
+  }
 }
