1ee4336a0f19270cb5f2b4a7c4981d7afac5639b
Ildiko Czeller <czeildi@gmail.com>
2020-12-31 17:39:10 +0100

    add 2019 day10 part 02

--- solutions_2019/day10_monitoring_station.R
+++ solutions_2019/day10_monitoring_station.R
@@ -11,14 +11,45 @@ asteroids <- tibble(location = read_lines("./solutions_2019/day10_input.txt")) %
   filter(location == "#") %>%
   select(x, y)
 
-crossing(station = asteroids, asteroid = asteroids) %>%
+asteroids_from_centers <- crossing(station = asteroids, asteroid = asteroids) %>%
   filter(station$x != asteroid$x | station$y != asteroid$y) %>%
   mutate(station_x = station$x, station_y = station$y, asteroid_x = asteroid$x, asteroid_y = asteroid$y) %>%
   select(station_x, station_y, asteroid_x, asteroid_y) %>%
   mutate(distance_x = asteroid_x - station_x, distance_y = asteroid_y - station_y) %>%
   rowwise() %>%
-  mutate(direction = str_c(c(distance_x, distance_y) / GCD(distance_x, distance_y), collapse = ";")) %>%
-  ungroup() %>%
+  mutate(n_step = GCD(distance_x, distance_y)) %>%
+  mutate(
+    direction_x = distance_x / n_step,
+    direction_y = distance_y / n_step
+  ) %>%
+  ungroup()
+
+# part 1 --------------------------------------------------------------------
+monitoring_station_coords <- asteroids_from_centers %>%
   group_by(station_x, station_y) %>%
-  summarise(n_visible_asteroid = n_distinct(direction)) %>%
-  arrange(desc(n_visible_asteroid))
+  summarise(n_visible_asteroid = n_distinct(glue::glue("{direction_x};{direction_y}"))) %>%
+  arrange(desc(n_visible_asteroid)) %>%
+  head(1) %>%
+  select(x = station_x, y = station_y, n_visible_asteroid)
+
+monitoring_station_coords$n_visible_asteroid
+
+# part 2 ---------------------------------------------------------------------
+
+asteroids_from_centers %>%
+  filter(station_x == monitoring_station_coords$x & station_y == monitoring_station_coords$y) %>%
+  select(direction_x, direction_y, n_step, asteroid_x, asteroid_y) %>%
+  group_by(direction_x, direction_y) %>%
+  arrange(n_step) %>%
+  mutate(tally_in_direction = 1:n()) %>%
+  rowwise() %>%
+  mutate(
+    rotation = case_when(
+      direction_x >= 0 ~ glue::glue("A:{direction_y / direction_x}"),
+      direction_x < 0 ~ glue::glue("B:{direction_y / direction_x}")
+    )
+  ) %>%
+  ungroup() %>%
+  arrange(tally_in_direction, rotation) %>%
+  mutate(laser_idx = 1:n()) %>%
+  filter(laser_idx == 200)
