6a746d04f4708877219642894169d55f02c61f0f
Ildiko Czeller <czeildi@gmail.com>
2021-12-08 07:25:05 +0100

    minor refactor on day 8

--- solutions_2021/day08.R
+++ solutions_2021/day08.R
@@ -4,18 +4,12 @@ library(tidyverse)
 
 tibble(x = read_lines("solutions_2021/day08_input.txt")) %>% 
   separate(x, into = c("input", "output"), sep = " \\| ") %>% 
-  select(output) %>% 
   separate_rows(output) %>% 
   mutate(x = nchar(output)) %>% 
-  filter(x == 2 | x == 4 | x == 3 | x == 7)
+  filter(x == 2 | x == 4 | x == 3 | x == 7) %>% 
+  nrow(.)
 
 # part 2 ------------------------------
-
-is_part_of <- function(s1, s2) {
-  chars1 <- str_split(s1, "")[[1]]
-  chars2 <- str_split(s2, "")[[1]]
-  all(chars1 %in% chars2)
-}
 
 size_of_intersection <- function(s1, s2) {
   chars1 <- str_split(s1, "")[[1]]
@@ -42,16 +36,16 @@ digits_in_order <- function(input) {
   d8 <- digits[nchar(digits) == 7]
   digits <- setdiff(digits, d8)
   
-  d9 <- keep(digits, ~nchar(.x) == 6 && is_part_of(d4, .x))
+  d9 <- keep(digits, ~nchar(.x) == 6 && size_of_intersection(d4, .x) == nchar(d4))
   digits <- setdiff(digits, d9)
   
-  d0 <- keep(digits, ~nchar(.x) == 6 && is_part_of(d1, .x))
+  d0 <- keep(digits, ~nchar(.x) == 6 && size_of_intersection(d1, .x) == nchar(d1))
   digits <- setdiff(digits, d0)
   
   d6 <- digits[nchar(digits) == 6]
   digits <- setdiff(digits, d6)
   
-  d3 <- keep(digits, ~nchar(.x) == 5 && is_part_of(d1, .x))
+  d3 <- keep(digits, ~nchar(.x) == 5 && size_of_intersection(d1, .x) == nchar(d1))
   digits <- setdiff(digits, d3)
   
   d2 <- keep(digits, ~nchar(.x) == 5 && size_of_intersection(.x, d4) == 2)
@@ -61,6 +55,7 @@ digits_in_order <- function(input) {
   
   in_order <- c(d0, d1, d2, d3, d4, d5, d6, d7, d8, d9)
   sorted_digits <- map_chr(in_order, sort_chars)
+  
   result <- 0:9
   names(result) <- sorted_digits
   result
