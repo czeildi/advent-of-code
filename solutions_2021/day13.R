f922b1d828a066091d91c4ac9123013d3718b432
Ildiko Czeller <czeildi@gmail.com>
2021-12-13 06:57:15 +0100

    some more refactor

--- solutions_2021/day13.R
+++ solutions_2021/day13.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
-input <- read_file("solutions_2021/input13.txt") %>% 
-  str_split("\n\n") %>% pluck(1)
+input <- str_split(read_file("solutions_2021/input13.txt"), "\n\n")[[1]]
 
 dots <- tibble(x = input[1]) %>% 
   separate_rows(x, sep = "\n") %>% 
@@ -13,17 +12,13 @@ instructions <- tibble(x = input[2]) %>%
   extract(x, c("axis", "value"), "fold along ([a-z])=(\\d+)", convert = TRUE)
 
 walk2(instructions$axis, instructions$value, function(axis, value) {
-  if (axis == 'x') {
-    dots <<- dots %>% 
-      mutate(x = if_else(x <= value, x, 2L * value - x)) %>% 
-      distinct(x, y)
-  } else {
-    dots <<- dots %>% 
-      mutate(y = if_else(y <= value, y, 2L * value - y)) %>% 
-      distinct(x, y)
-  }
+  dots <<- dots %>% 
+    mutate(
+      x = if_else(x <= value | axis == 'y', x, 2L * value - x),
+      y = if_else(y <= value | axis == 'x', y, 2L * value - y)
+    ) %>% 
+    distinct(x, y)
 })
 
-dots %>% 
-  ggplot(aes(x = x, y = -y)) + 
+ggplot(dots, aes(x = x, y = -y)) + 
   geom_tile()
