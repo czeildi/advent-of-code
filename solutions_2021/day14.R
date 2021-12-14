acdf172a71b3f4dd7b34d7b8ec0ba258f63c81e1
Ildiko Czeller <czeildi@gmail.com>
2021-12-14 07:19:51 +0100

    add day14 part 2

--- solutions_2021/day14.R
+++ solutions_2021/day14.R
@@ -4,32 +4,44 @@ input <- str_split(read_file("solutions_2021/input14.txt"), "\n\n")[[1]]
 
 starting <- input[1]
 
-# part 1
-
 rules <- tibble(x = input[2]) %>% 
   separate_rows(x, sep = "\n") %>% 
   filter(x != "") %>% 
   extract(x, c("pair", "inserted"), "([A-Z]+) -> ([A-Z])", convert = TRUE) %>% 
   rowwise() %>% 
-  mutate(inserted = paste0(str_split(pair, "")[[1]][1], inserted, str_split(pair, "")[[1]][2])) %>% 
+  mutate(new_pairs = list(c(
+    paste0(str_split(pair, "")[[1]][1], inserted),
+    paste0(inserted, str_split(pair, "")[[1]][2]))
+  )) %>% 
   ungroup() %>% 
-  deframe()
+  select(-inserted)
+
 
 pairs_from_string <- function(text) {
   text_chars <- str_split(text, "")[[1]]
   head(paste0(text_chars, lead(text_chars)), -1)
 }
 
-string_from_pieces <- function(pieces) {
-  map_chr(pieces, ~paste(str_split(., "")[[1]][1:2], collapse = "")) %>% 
-    paste(collapse = "") %>% 
-    paste0(., str_split(tail(pieces, 1), "")[[1]][3])
-}
+initial_pair_frequencies <- table(pairs_from_string(starting)) %>% 
+  enframe(name = "pair", value = 'freq') %>% 
+  mutate(freq = as.numeric(freq))
 
-final <- reduce(1:10, function(prev, current) {
-  string_from_pieces(rules[pairs_from_string(prev)])
-}, .init = starting)
+final_pair_frequencies <- reduce(1:40, function(prev, current) {
+  prev %>% 
+    inner_join(rules) %>% 
+    unnest_longer(new_pairs) %>% 
+    group_by(new_pairs) %>% 
+    summarize(freq = sum(freq)) %>% 
+    rename(pair = new_pairs)
+}, .init = initial_pair_frequencies)
 
-freqs <- table(str_split(final, "")[[1]])
+final_letter_frequencies <- final_pair_frequencies %>% 
+  rowwise() %>% 
+  mutate(letters = list(str_split(pair, "")[[1]])) %>% 
+  unnest_longer(letters) %>% 
+  group_by(letters) %>% 
+  summarize(freq = ceiling(sum(freq)/2)) %>% 
+  arrange(freq)
 
-max(freqs) - min(freqs)
+options(scipen=99)
+tail(final_letter_frequencies$freq, 1) - head(final_letter_frequencies$freq, 1)
