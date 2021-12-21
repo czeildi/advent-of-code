cd29ef0de96ff0a4127b1d630ea47f8310f3e167
Ildiko Czeller <czeildi@gmail.com>
2021-12-21 10:18:30 +0100

    add utils

---
+++ utils.R
@@ -0,0 +1,35 @@
+one_indexed_remainder <- function(value, mod) {
+  if_else(value %% mod == 0, mod, value %% mod) 
+}
+
+directions <- tribble(
+  ~direction, ~dx, ~dy,
+  "tl", -1, -1,
+  "t", 0, -1,
+  "tr", 1, -1,
+  "l", -1, 0,
+  "self", 0, 0,
+  "r", 1, 0,
+  "bl", -1, 1,
+  "b", 0, 1,
+  "br", 1, 1
+) %>% 
+  select(-direction)
+
+binary_vec_to_decimal <- function(binary) {
+  sum(rev(binary) * 2 ^ (0:(length(binary) - 1)))
+}
+
+decimal_to_n_digit_binary <- function(num, n) {
+  result <- num %% 2
+  num <- (num - result) / 2
+  while (num > 0) {
+    result <- paste0(num %% 2, result)
+    num <- (num - num %% 2) / 2
+  }
+  str_pad(result, n, pad = '0')
+}
+
+sort_chars <- function(word) {
+  paste(sort(str_split(word, "")[[1]]), collapse = "")
+}
