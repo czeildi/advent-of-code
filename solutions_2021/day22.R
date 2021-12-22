1c209231bb2150dddfa4f26aa0434a6945055b0a
Ildiko Czeller <czeildi@gmail.com>
2021-12-22 06:21:34 +0100

    add part 1 simplified

--- solutions_2021/day22.R
+++ solutions_2021/day22.R
@@ -1,14 +1,10 @@
 library(tidyverse)
 
 input <- tibble(value = read_lines("solutions_2021/input22.txt")) %>% 
-  extract(value, c("method", "x1", "x2", "y1", "y2", "z1", "z2"), regex = "([a-z]+) x=([0-9-]+)..([0-9-]+),y=([0-9-]+)..([0-9-]+),z=([0-9-]+)..([0-9-]+)", convert = TRUE)
+  extract(value, c("method", "x1", "x2", "y1", "y2", "z1", "z2"), regex = "([a-z]+) x=([0-9-]+)..([0-9-]+),y=([0-9-]+)..([0-9-]+),z=([0-9-]+)..([0-9-]+)", convert = TRUE) %>% 
+  mutate(method = as.integer(method == "on"))
 
-follow_lighting <- function(method_sequence) {
-  reduce(method_sequence, function(prev, current) {
-    if(current == "on") return(1);
-    if(current == "off") return(0);
-  }, .init = 0)
-}
+# part 1
 
 input %>% 
   filter(x1 >= -50 & y1 >= -50 & z1 >= -50 & x2 <= 50 & y2 <= 50 & z2 <= 50) %>%
@@ -18,7 +14,11 @@ input %>%
   select(x, y, z, method) %>% 
   group_by(x, y, z) %>% 
   summarize(
-    final_value = follow_lighting(method)
+    final_value = tail(method, 1)
   ) %>% 
   ungroup() %>% 
   summarize(num_turned_on = sum(final_value))
+
+# part 2
+
+lights <- head(input, 1)
