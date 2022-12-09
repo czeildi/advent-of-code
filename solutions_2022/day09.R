52f9a41875379d5d1e21b4c49931ff24a0d4b9ac
Ildiko Czeller <czeildi@gmail.com>
2022-12-09 08:51:11 +0100

    add 2022 day 9 part 2

--- solutions_2022/day09.R
+++ solutions_2022/day09.R
@@ -16,7 +16,7 @@ head_steps <- list(
   "U" = c(0, -1)
 )
 
-tail_steps <- list(
+knot_steps <- list(
   "0;2" = c(0, 1),
   "0;-2" = c(0, -1),
   "2;0" = c(1, 0),
@@ -28,23 +28,32 @@ tail_steps <- list(
   "-1;2" = c(-1, 1),
   "-1;-2" = c(-1, -1),
   "2;-1" = c(1, -1),
-  "-2;-1" = c(-1, -1)
+  "-2;-1" = c(-1, -1),
+  "2;2" = c(1, 1),
+  "-2;-2" = c(-1, -1),
+  "2;-2" = c(1, -1),
+  "-2;2" = c(-1, 1)
 )
 
-h_chain <- c(0, 0)
-t_chain <- c(0, 0)
-visited <- list("0;0" = TRUE)
+knot_positions <- lapply(1:10, \(i) c(0, 0))
+tail_visited <- list("0;0" = TRUE)
 
 for (step in steps) {
   head_step <- head_steps[[step[1]]]
   for (i in seq_len(step[2])) {
-    h_chain <- h_chain + head_step
-    position_difference <- paste(h_chain - t_chain, collapse = ";")
-    if (position_difference %in% names(tail_steps)) {
-      t_chain <- t_chain + tail_steps[[position_difference]]
-      visited[[paste(t_chain, collapse = ";")]] <- TRUE
+    knot_positions[[1]] <- knot_positions[[1]] + head_step
+
+    for (knot_idx in 2:10) {
+      position_difference <- paste(
+        knot_positions[[knot_idx - 1]] - knot_positions[[knot_idx]],
+        collapse = ";"
+      )
+      if (position_difference %in% names(knot_steps)) {
+        knot_positions[[knot_idx]] <- knot_positions[[knot_idx]] + knot_steps[[position_difference]]
+      }
     }
+    tail_visited[[paste(knot_positions[[10]], collapse = ";")]] <- TRUE
   }
 }
 
-length(visited)
+length(tail_visited)
