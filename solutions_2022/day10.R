3e9bbf07a3cd2dcc7c1f58d246f0968f229bea70
Ildiko Czeller <czeildi@gmail.com>
2022-12-10 11:02:36 +0100

    small refactor

--- solutions_2022/day10.R
+++ solutions_2022/day10.R
@@ -12,23 +12,23 @@ input <- tibble(x = read_lines(input_file))
 commands <- input |>
   separate(x, into = c("cmd", "dx"), sep = " ", convert = TRUE, fill = "right") |>
   mutate(idx = cumsum(if_else(cmd == "addx", 2L, 1L)) + 1) |>
-  mutate(dx = if_else(is.na(dx), 0L, dx))
+  mutate(dx = replace_na(dx, 0))
 
 dummy <- commands |>
   filter(cmd == "addx") |>
   mutate(dx = 0, idx = idx - 1)
 
-# part 1
-rbind(commands, dummy) |>
+x_positions <- rbind(commands, dummy) |>
   arrange(idx) |>
-  mutate(value = cumsum(dx) + 1) |>
+  mutate(x_pos = cumsum(dx) + 1)
+
+# part 1
+x_positions |>
   filter(idx %% 40 == 20) |>
-  summarize(sum(value * idx))
+  summarize(sum(x_pos * idx))
 
 # part 2
-rbind(commands, dummy) |>
-  arrange(idx) |>
+x_positions |>
   mutate(x = (row_number() - 1) %% 40, y = (row_number() - 1) %/% 40) |>
-  mutate(value = cumsum(dx) + 1) |>
-  filter(abs(x - lag(value, default = 1)) <= 1) |>
+  filter(abs(x - lag(x_pos, default = 1)) <= 1) |>
   ggplot(aes(x, -y)) + geom_tile()
