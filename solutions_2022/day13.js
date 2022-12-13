93c2e4ff1ade80fab4a7a43d62edb76c06fea75a
Ildiko Czeller <czeildi@gmail.com>
2022-12-13 09:17:16 +0100

    add day13 part 2

--- solutions_2022/day13.js
+++ solutions_2022/day13.js
@@ -2,15 +2,15 @@
 
 const assert = require('assert');
 const fs = require('fs');
-const { tail, sum } = require('lodash');
+const { tail, sum, isEqual } = require('lodash');
 
 const pairs = fs.readFileSync('./solutions_2022/day13_input.txt', 'utf8').split('\n\n')
 
 const compare = (left, right) => {
-  if (left.length === 0 & right.length === 0) return 'eq'
+  if (left.length === 0 & right.length === 0) return 0
 
   if (left.length === 0 | right.length === 0) {
-    return left.length < right.length;
+    return left.length < right.length ? -1 : 1;
   }
   const leftValue = left[0]
   const rightValue = right[0]
@@ -19,36 +19,37 @@ const compare = (left, right) => {
     if (leftValue === rightValue) {
       return compare(tail(left), tail(right))
     }
-    return leftValue < rightValue
+    return leftValue < rightValue ? -1 : 1;
   }
   if (Array.isArray(leftValue) & Array.isArray(rightValue)) {
     const res = compare(leftValue, rightValue)
-    if (res === 'eq') {
+    if (res === 0) {
       return compare(tail(left), tail(right))
     }
     return res
   }
   if (Array.isArray(leftValue) & !Array.isArray(rightValue)) {
     const res = compare(leftValue, [rightValue])
-    if (res === 'eq') {
+    if (res === 0) {
       return compare(tail(left), tail(right))
     }
     return res
   }
   if (!Array.isArray(leftValue) & Array.isArray(rightValue)) {
     const res = compare([leftValue], rightValue)
-    if (res === 'eq') {
+    if (res === 0) {
       return compare(tail(left), tail(right))
     }
     return res
   }
 }
 
-assert(compare([], [[]]))
-assert(compare([], [1]))
-assert(compare(3, 5))
-assert(compare([[1]], [[1]]))
+assert(compare([], []) === 0)
+assert(compare([], [[]]) === -1)
+assert(compare([], [1]) === -1)
+assert(compare([[1]], [[1]]) === 0)
 
+// part 1
 const res = pairs.map((pair, idx) => {
   const [leftRaw, rightRaw] = pair.split('\n')
   const [left, right] = [leftRaw, rightRaw].map(JSON.parse)
@@ -57,4 +58,19 @@ const res = pairs.map((pair, idx) => {
   return [idx, compare(left, right)]
 })
 
-console.log(sum(res.filter(r => r[1]).map(r => r[0] + 1)))
+console.log(sum(res.filter(r => r[1] === -1).map(r => r[0] + 1)))
+
+// part 2
+const elements = [[[2]], [[6]]]
+
+for (const pair of pairs) {
+  const [leftRaw, rightRaw] = pair.split('\n')
+  elements.push(JSON.parse(leftRaw))
+  elements.push(JSON.parse(rightRaw))
+}
+
+const sorted = elements.sort(compare)
+
+const index_2 = sorted.findIndex(val => isEqual(val, [[2]])) + 1
+const index_6 = sorted.findIndex(val => isEqual(val, [[6]])) + 1
+console.log(index_2 * index_6)
