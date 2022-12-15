1df12d87619ae91d952f618367aa77dc46aefa21
Ildiko Czeller <czeildi@gmail.com>
2022-12-15 09:26:01 +0100

    add day15 part 2

--- solutions_2022/day15.R
+++ solutions_2022/day15.R
@@ -7,18 +7,20 @@ library(ivs)
 year <- "2022"
 day <- "15"
 input_file <- glue("solutions_{year}/day{day}_input.txt")
-
-target_y <- 2000000
-
 input <- tibble(x = read_lines(input_file))
 
-intervals <- input |>
+sensors_and_beacons <- input |>
   extract(
     x, c("xs", "ys", "xb", "yb"),
     "Sensor at x=([0-9-]+), y=([0-9-]+): closest beacon is at x=([0-9-]+), y=([0-9-]+)",
     convert = TRUE
   ) |>
-  mutate(manh = abs(xs - xb) + abs(ys - yb)) |>
+  mutate(manh = abs(xs - xb) + abs(ys - yb))
+
+# part 1
+target_y <- 2000000
+
+sensors_and_beacons |>
   mutate(dist_y = abs(ys - target_y)) |>
   mutate(
     left = if_else(manh - dist_y < 0, NA_real_, xs - (manh - dist_y)),
@@ -26,10 +28,37 @@ intervals <- input |>
   ) |>
   filter(!is.na(left)) |>
   mutate(target_iv = iv(left, right)) |>
-  pull(target_iv)
-
-tibble(x = iv_groups(intervals)) |>
-  mutate(s = iv_start(x), e = iv_end(x)) |>
+  summarise(union_intervals = iv_groups(target_iv)) |>
+  mutate(s = iv_start(union_intervals), e = iv_end(union_intervals)) |>
   mutate(l = e - s - 1) |>
   pull(l) |>
   sum()
+
+# part 2
+
+xs <- sensors_and_beacons$xs
+ys <- sensors_and_beacons$ys
+manh <- sensors_and_beacons$manh
+grid_max <- 4000000
+
+system.time(
+res <- invisible(lapply(0:grid_max, function(grid_y) {
+  if (grid_y %% 10000 == 0) message(Sys.time(), ": ", grid_y)
+
+  dist_y <- abs(ys - grid_y)
+  left <- if_else(manh - dist_y < 0, NA_real_, pmax(xs - (manh - dist_y), 0))
+  right <- if_else(manh - dist_y < 0, NA_real_, pmin(xs + (manh - dist_y) + 1, grid_max + 1))
+  valid <- !is.na(left) & !is.na(right) & left < right
+
+  ui <- iv_groups(iv(left[valid], right[valid]))
+
+  if (length(ui) == 1) return(NULL)
+
+  distress <- iv_start(iv_complement(ui))
+  result <- distress * 4000000 + grid_y
+  cat("\n\nResult:", result, "\n\n")
+  result
+}))
+)
+
+compact(res)[[1]]
