1010f6c587c0969b6b432d09a74153c9933d58bb
Ildiko Czeller <czeildi@gmail.com>
2022-12-21 09:23:22 +0100

    add day21 part 2

--- solutions_2022/day21.R
+++ solutions_2022/day21.R
@@ -36,7 +36,58 @@ exprs_in_topo_order <- nodes_in_topo_order |>
   arrange(topo_order) |>
   deframe()
 
+# part 1
 for (expr in exprs_in_topo_order) {
   eval(parse(text = expr))
 }
 root
+
+# part 2
+
+parsed <- input |>
+  extract(x, c("name", "yell"), regex = "(.*): (.*)") |>
+  mutate(
+    node_id = row_number(),
+    expr = paste(name, yell, sep = " <- ")
+  ) |>
+  mutate(yell = case_when(
+    name == "humn" ~ "humn",
+    name == "root" ~ sub("\\+", "-", yell),
+    TRUE ~ yell
+  ))
+
+nodes <- select(parsed, node_id, name)
+edges <- parsed |>
+  filter(is.na(as.integer(yell))) |>
+  separate_rows(yell) |>
+  select(from = name, to = yell)
+
+g <- tbl_graph(nodes, edges)
+
+nodes_in_topo_order <- g |>
+  activate(nodes) |>
+  mutate(topo_order = node_topo_order()) |>
+  activate(nodes) |>
+  as_tibble() |>
+  arrange(topo_order)
+
+exprs_in_topo_order <- nodes_in_topo_order |>
+  left_join(parsed, by = c("node_id", "name")) |>
+  arrange(topo_order) |>
+  select(name, yell) |>
+  deframe()
+
+library(symengine)
+
+vars <- lapply(parsed$name, as.name)
+names(vars) <- parsed$name
+do.call(use_vars, vars)
+
+expr <- S(exprs_in_topo_order[["root"]])
+
+for (var_name in names(exprs_in_topo_order)) {
+  expr <- subs(expr, S(var_name), S(exprs_in_topo_order[[var_name]]))
+  expr
+}
+
+solve(expr, "humn")
