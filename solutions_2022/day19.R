e8dc24057c9e9d0bd859a065ae9c50a1b76db8e5
Ildiko Czeller <czeildi@gmail.com>
2022-12-22 17:36:23 +0100

    add day19 part 1

--- solutions_2022/day19.R
+++ solutions_2022/day19.R
@@ -18,44 +18,56 @@ blueprints <- input |>
   nest_by(id, .keep = TRUE) |>
   deframe()
 
-res <- 0
-lapply(seq_along(blueprints), function(blueprint_id) {
-
-bp <- blueprints[[blueprint_id]]$cost
-costs <- rbind(
-  c(-bp[1], -bp[2], -bp[3], -bp[5]),
-  c(0, 0, -bp[4], 0),
-  c(0, 0, 0, -bp[6]),
-  c(0, 0, 0, 0)
-)
-
 production <- function(bought, now) {
   max(now - bought - 1, 0)
 }
 
 max_t <- 24
 
-lp_coeffs <- lapply(seq_len(max_t), function(now) {
-  lapply(seq_len(max_t), function(bought) {
-    # the future does not affect us
-    if (bought > now) return(diag(0, 4, 4))
-    costs + diag(production(bought, now), 4, 4)
-  }) |> do.call(cbind, args = _)
-}) |> do.call(rbind, args = _)
+res <- 0
 
-lp_lower_bounds <- lapply(seq_len(max_t), function(t) c(-t + 1, 0, 0, 0)) |>
-  do.call(c, args = _)
+lapply(seq_along(blueprints), function(blueprint_id) {
 
-max_coeffs <- lapply(seq_len(max_t), function(t) c(0, 0, 0, max_t - t)) |>
-  do.call(c, args = _)
+  bp <- blueprints[[blueprint_id]]$cost
+  costs <- rbind(
+    c(-bp[1], -bp[2], -bp[3], -bp[5]),
+    c(0, 0, -bp[4], 0),
+    c(0, 0, 0, -bp[6]),
+    c(0, 0, 0, 0)
+  )
 
-system.time({
-max_geode <- lp("max", max_coeffs, lp_coeffs, rep(">=", max_t * 4), lp_lower_bounds, all.bin = TRUE)
+  has_money_for_buying <- lapply(seq_len(max_t), function(now) {
+    lapply(seq_len(max_t), function(bought) {
+      # the future does not affect us
+      if (bought > now) return(diag(0, 4, 4))
+      costs + diag(production(bought, now), 4, 4)
+    }) |> do.call(cbind, args = _)
+  }) |> do.call(rbind, args = _)
+
+  one_robot_per_minute <- lapply(seq_len(max_t), function(now) {
+    lapply(seq_len(max_t), function(t) {
+      if (t != now) return(matrix(c(0, 0, 0, 0), nrow = 1))
+      return(matrix(c(-1, -1, -1, -1), nrow = 1))
+    }) |> do.call(cbind, args = _)
+  }) |> do.call(rbind, args = _)
+
+  lp_coeffs <- rbind(has_money_for_buying, one_robot_per_minute)
+
+  lp_lower_bounds <- lapply(seq_len(max_t), function(t) c(-t + 1, 0, 0, 0)) |>
+    do.call(c, args = _)
+  lp_lower_bounds <- c(lp_lower_bounds, rep(-1, max_t))
+
+  max_coeffs <- lapply(seq_len(max_t), function(t) c(0, 0, 0, max_t - t)) |>
+    do.call(c, args = _)
+
+  max_geode <- lp(
+    "max",
+    max_coeffs, lp_coeffs, rep(">=", max_t * 5), lp_lower_bounds,
+    all.bin = TRUE
+  )
+
+  cat(blueprint_id, ": ", max_geode$objval, "", as.character(Sys.time()), "\n")
+  res <<- res + blueprint_id * max_geode$objval
 })
-
-cat(blueprint_id, ": ", max_geode$objval,"" , as.character(Sys.time()), "\n")
-res <<- res + blueprint_id * max_geode$objval
-})
-
 
 res
