d81d81962e69235358a44abbcb2713b9db775dac
Ildiko Czeller <czeildi@gmail.com>
2025-03-28 21:04:27 +0100

    add air formatter config
    mainly trying it out

--- global_leaderboard_analysis.R
+++ global_leaderboard_analysis.R
@@ -5,7 +5,7 @@ library(glue)
 year <- 2020
 csv_filename <- glue("leaderboard_data/global_leaderboard_{year}.csv")
 # leaderboard_pages <- map(1:25, ~read_html(glue("https://adventofcode.com/{year}/leaderboard/day/{.}")))
-# 
+#
 # leaderboard_entries <- imap(leaderboard_pages, ~{
 #   .x %>%
 #     html_nodes('body') %>%
@@ -18,11 +18,11 @@ csv_filename <- glue("leaderboard_data/global_leaderboard_{year}.csv")
 #       into = c("place", "day_idx", "time", "user_name"),
 #       regex = "(\\d+)\\) Dec (\\d+)  (\\d\\d:\\d\\d:\\d\\d)  (.*)"
 #     ) %>%
-#     mutate(day_idx = .y, type = if_else(idx %% 200 <= 100, "gold", "silver")) %>% 
+#     mutate(day_idx = .y, type = if_else(idx %% 200 <= 100, "gold", "silver")) %>%
 #     select(-idx)
 # }) %>%
 #   reduce(rbind)
-# 
+#
 # write_csv(leaderboard_entries, csv_filename)
 
 leaderboard_entries <- read_csv(csv_filename) %>%
@@ -32,8 +32,8 @@ n_distinct(leaderboard_entries$user_name)
 
 theme_set(theme_bw())
 
-ggplot(leaderboard_entries, aes(x = day_idx, y = time)) + 
-  geom_boxplot() + 
+ggplot(leaderboard_entries, aes(x = day_idx, y = time)) +
+  geom_boxplot() +
   facet_wrap(vars(type), ncol = 1, scales = "free_y")
 
 part_comparison <- inner_join(
@@ -42,9 +42,9 @@ part_comparison <- inner_join(
   by = c("user_name", "day_idx")
 )
 
-ggplot(part_comparison, aes(x = place.x, y = place.y)) + 
-  geom_point(alpha = 0.2) + 
+ggplot(part_comparison, aes(x = place.x, y = place.y)) +
+  geom_point(alpha = 0.2) +
   geom_smooth(method = "lm")
 
-ggplot(part_comparison, aes(x = day_idx, y = as.integer(time.y - time.x))) + 
+ggplot(part_comparison, aes(x = day_idx, y = as.integer(time.y - time.x))) +
   geom_boxplot()
--- private_leaderboard_analysis.R
+++ private_leaderboard_analysis.R
@@ -6,21 +6,24 @@ library(ggrepel)
 # prepare data ------------------------------------------------------------
 PERSON_STAR_LIMIT <- 25
 
-daily_publish_times <- (seq.Date(as.Date("2020-12-01"), as.Date("2020-12-25"), by = "1 day") + lubridate::duration(6, 'hours')) %>%
+daily_publish_times <- (seq.Date(as.Date("2020-12-01"), as.Date("2020-12-25"), by = "1 day") +
+  lubridate::duration(6, 'hours')) %>%
   set_names(seq_along(.)) %>%
   enframe("day_idx", "publish_ts")
 
-stats <- jsonlite::read_json("leaderboard_data/private_1226.json") %>% 
+stats <- jsonlite::read_json("leaderboard_data/private_1226.json") %>%
   .[['members']] %>%
-  discard(~is.null(.[["name"]])) %>%
-  discard(~.[['local_score']] == 0) %>%
+  discard(~ is.null(.[["name"]])) %>%
+  discard(~ .[['local_score']] == 0) %>%
   set_names(map_chr(., "name"))
 
 part_completions <- stats %>%
   map("completion_day_level") %>%
   enframe("name", "completion_day_level") %>%
   rowwise() %>%
-  mutate(completion_day_level = list(enframe(completion_day_level, "day_idx", "part_completions"))) %>%
+  mutate(
+    completion_day_level = list(enframe(completion_day_level, "day_idx", "part_completions"))
+  ) %>%
   unnest(completion_day_level) %>%
   rowwise() %>%
   mutate(part_completions = list(enframe(part_completions, "part_idx", "completion_ts"))) %>%
@@ -41,69 +44,80 @@ diff_between_parts_stat <- part_completions %>%
   filter(n_star >= PERSON_STAR_LIMIT)
 
 diff_between_parts_stat %>%
-  mutate(name = fct_reorder(name, diff_between_parts, .fun = median)) %>% 
-  ggplot(aes(x = name, y = diff_between_parts)) + 
-  geom_point(size = 3, alpha = 0.2) + 
+  mutate(name = fct_reorder(name, diff_between_parts, .fun = median)) %>%
+  ggplot(aes(x = name, y = diff_between_parts)) +
+  geom_point(size = 3, alpha = 0.2) +
   geom_text_repel(
     data = filter(diff_between_parts_stat, diff_between_parts >= 12 * 3600),
     aes(label = day_idx)
-  ) + 
+  ) +
   stat_summary(fun = mean, geom = "point", shape = 21, size = 6, color = "red") +
   stat_summary(fun = median, geom = "point", shape = 21, size = 6, color = "darkgreen") +
   scale_y_sqrt(
-    breaks = c(600, 3600, 6 * 3600, 24*3600),
+    breaks = c(600, 3600, 6 * 3600, 24 * 3600),
     labels = c('10min', '1h', '6h', '1day')
-  ) + 
-  coord_flip() + 
-  labs(x = NULL, y = 'time between solving part1 and part2', caption = "Green:  median\nRed: average") + 
+  ) +
+  coord_flip() +
+  labs(
+    x = NULL,
+    y = 'time between solving part1 and part2',
+    caption = "Green:  median\nRed: average"
+  ) +
   theme_bw()
 
 plot_completion <- function(df) {
   df %>%
     mutate(name = fct_reorder(name, completion_seconds)) %>%
     ggplot(aes(x = name, y = completion_seconds)) +
-    geom_point(size = 3, alpha = 0.2) + 
+    geom_point(size = 3, alpha = 0.2) +
     stat_summary(fun = mean, geom = "point", shape = 21, size = 6, color = "red") +
-    stat_summary(fun = median, geom = "point", shape = 21, size = 6, color = "darkgreen") + 
+    stat_summary(fun = median, geom = "point", shape = 21, size = 6, color = "darkgreen") +
     scale_y_sqrt(
-      breaks = c(600, 3600, 6 * 3600, 24*3600),
+      breaks = c(600, 3600, 6 * 3600, 24 * 3600),
       labels = c('10min', '1h', '6h', '1day')
-    ) + 
-    coord_flip() + 
-    labs(x = NULL, y = 'time to solve', caption = "Green:  median\nRed: average") + 
+    ) +
+    coord_flip() +
+    labs(x = NULL, y = 'time to solve', caption = "Green:  median\nRed: average") +
     theme_bw()
 }
 
-plot_completion(part_completions %>% filter(n_star >= PERSON_STAR_LIMIT & part_idx == 1)) + 
+plot_completion(part_completions %>% filter(n_star >= PERSON_STAR_LIMIT & part_idx == 1)) +
   plot_completion(part_completions %>% filter(n_star >= PERSON_STAR_LIMIT & part_idx == 2)) +
   plot_annotation(tag_levels = 1)
 
 part_completions %>%
   filter(n_star >= PERSON_STAR_LIMIT) %>%
-  group_by(name, part_idx) %>% 
-  summarize(median_completion_seconds = median(completion_seconds)) %>% 
-  group_by(part_idx) %>% 
-  mutate(rank_for_part = dplyr::min_rank(median_completion_seconds), part_idx = as.character(part_idx)) %>% 
-  ggplot(aes(x = part_idx, y = rank_for_part)) + 
-  geom_line(aes(group = name, color = name)) + 
-  geom_point(aes(color = name)) + 
-  geom_text(aes(x = part_idx, y = rank_for_part, label = rank_for_part), nudge_x = 0.03) + 
-  theme_minimal() + 
+  group_by(name, part_idx) %>%
+  summarize(median_completion_seconds = median(completion_seconds)) %>%
+  group_by(part_idx) %>%
+  mutate(
+    rank_for_part = dplyr::min_rank(median_completion_seconds),
+    part_idx = as.character(part_idx)
+  ) %>%
+  ggplot(aes(x = part_idx, y = rank_for_part)) +
+  geom_line(aes(group = name, color = name)) +
+  geom_point(aes(color = name)) +
+  geom_text(aes(x = part_idx, y = rank_for_part, label = rank_for_part), nudge_x = 0.03) +
+  theme_minimal() +
   scale_y_continuous(breaks = 1:10)
 
 diff_between_parts_stat %>%
   mutate(day_idx = fct_reorder(day_idx, diff_between_parts, .fun = median)) %>%
-  ggplot(aes(x = day_idx, y = diff_between_parts)) + 
-  geom_point(size = 3, alpha = 0.2) + 
+  ggplot(aes(x = day_idx, y = diff_between_parts)) +
+  geom_point(size = 3, alpha = 0.2) +
   # geom_text_repel(
   #   data = filter(diff_between_parts_stat, diff_between_parts >= 12 * 3600),
   #   aes(label = name)
-  # ) + 
+  # ) +
   stat_summary(fun = mean, geom = "point", shape = 21, size = 6, color = "red") +
   stat_summary(fun = median, geom = "point", shape = 21, size = 6, color = "darkgreen") +
   scale_y_sqrt(
-    breaks = c(600, 3600, 6 * 3600, 24*3600),
+    breaks = c(600, 3600, 6 * 3600, 24 * 3600),
     labels = c('10min', '1h', '6h', '1day')
-  ) + 
-  labs(x = NULL, y = 'time between solving part1 and part2', caption = "Green:  median\nRed: average") + 
+  ) +
+  labs(
+    x = NULL,
+    y = 'time between solving part1 and part2',
+    caption = "Green:  median\nRed: average"
+  ) +
   theme_bw()
